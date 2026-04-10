e2d7e0a5293958b04b05c64610eddeed5c34c547
adria <adria@REX54X>
2026-04-10 08:28:35 -0600

    preva 1

---
+++ Estudiantes/Program.cs
@@ -0,0 +1,24 @@
+﻿Estudiante e = new Estudiante();
+e.Edad = 98;
+class Estudiante
+{
+    private double edad;
+
+    public double Edad
+    {
+        get { return edad; }
+        set
+        {
+            if (value > 0 && value <= 100)
+            {
+                Console.WriteLine("Edad valida");
+                edad = value;
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+
+}
