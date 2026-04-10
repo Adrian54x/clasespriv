3ffe8ab0df8b3e05b93c08733113ed5a2356c028
adria <adria@REX54X>
2026-04-10 08:43:09 -0600

    prueba 1

---
+++ Empleado/Program.cs
@@ -0,0 +1,24 @@
+﻿Empleados e = new Empleados();
+e.Saldo = 2000;
+class Empleados
+{
+    private double saldo;
+
+    public double Saldo
+    {
+        get { return saldo; }
+        set
+        {
+            if (value > 1500)
+            {
+                Console.WriteLine("Salario valido");
+                saldo = value;
+            }
+            else
+            {
+                Console.WriteLine("Salario invalido");
+            }
+        }
+    }
+
+}
