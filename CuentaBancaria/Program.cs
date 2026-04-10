1aaf97b63315f0aeb07aa9dc949d12f1faee2356
adria <adria@REX54X>
2026-04-10 08:18:26 -0600

    prueba 1

---
+++ CuentaBancaria/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Ingrese su saldo:");
+Cuentas c = new Cuentas();
+double x;
+if(double.TryParse(Console.ReadLine(), out x))
+{
+	c.Saldo = x;
+}
+else
+{
+    Console.WriteLine("Dato no valido!");
+}
+class Cuentas
+{
+	private double saldo;
+
+	public  double Saldo
+	{
+		get { return saldo; }
+		set 
+		{ 
+			if (value > 0) 
+			{
+                Console.WriteLine("Saldo realizado");
+				saldo = value; 
+			} 
+			else
+			{
+                Console.WriteLine("Saldo no aceptado!");
+			}
+		}
+	}
+
+}
